cd7cc9368805244b72fd4a61fe66880ae0da1e09
Tavis Tucker <ttucker@tenable.com>
2022-12-13 15:02:38 -0500

    Tweak content

--- pages/services.tsx
+++ pages/services.tsx
@@ -24,17 +24,17 @@ export default function Resume() {
             <h4>Here&#39;s how I can help you.</h4>
             <p>Whether you&#39;re an interactive media agency looking for a full-time developer or a small business owner that needs an
                 online presence, we should talk. Though my primary area of <Link key="projects" href="/projects">experience</Link> and interest lies in translating
-                static website designs into dynamic user interfaces, I could:</p>
+                static designs into dynamic user interfaces, I could:</p>
             <ul>
                 <li>Create a responsive web-based single page application</li>
-                <li>Redesign and/or build a new website with a CMS</li>
+                <li>Redesign and/or build a new website with a CMS backend</li>
                 <li>Provide recommendations to improve user experience</li>
                 <li>Help architect a website development project</li>
                 <li>Add search engine optimization or analytics solutions</li>
-                <li>Implement a blogging platform or build a Facebook app</li>
-                <li>Bring your new and interesting idea to life!</li>
+                <li>Implement a blogging platform to give you a voice</li>
+                <li>Bring your new and interesting idea to life</li>
             </ul>
-            <p>Need help? <Link key="contact" href="/contact">Get in touch</Link></p>
+            <p>Need help? <Link key="contact" href="/contact">Get in touch</Link> and let&#39;s start a conversation!</p>
         </article>
       </main>
       <Footer />
