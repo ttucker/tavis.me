74ea1faef3ee987d260f4569804226cb76e90242
Tavis Tucker <ttucker@tenable.com>
2023-01-10 11:19:05 -0500

    add lang to html tag for better a11y

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en-US">
       <Head>
         <meta name="description" content="This is tavis.me, the professional portfolio of Tavis Tucker, an internet geek currently living near Washington, DC.
             The intent is to showcase Tavis Tucker's skills, highlight project experience and give prospective clients or employers ways to get in touch." />
