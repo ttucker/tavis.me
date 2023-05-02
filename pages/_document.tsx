2b8da1e664c394537b191f8e8041afb728572322
Tavis Tucker <tavis.tucker@runzero.com>
2023-05-02 11:20:05 -0400

    Update dependencies; add Google Analytics code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,6 +8,20 @@ export default function Document() {
         <meta name="description" content="This is tavis.me, the professional portfolio of Tavis Tucker, an internet geek currently living near Washington, DC.
             The intent is to showcase Tavis Tucker's skills, highlight project experience and give prospective clients or employers ways to get in touch." />
         <link rel="icon" href="/favicon.ico" />
+        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-B0SGJGWJ42"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-B0SGJGWJ42');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
