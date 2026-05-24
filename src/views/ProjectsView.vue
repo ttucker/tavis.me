7983b7008546b41bbad77e8a4c8344a85e25d717
Tavis Tucker <tavis.tucker@yahoo.com>
2026-05-24 00:05:59 -0400

    add carousel to project page

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,147 +3,158 @@
     <HeroImage current-page="projects" img-src="/projects.jpg" />
     <main>
       <ContentArticle>
-      <h3>Projects</h3>
+        <h3>Projects</h3>
 
-      <h4>@Tenable</h4>
-      <p>I was fortunate to have many opportunities at Tenable to bootstrap several new features and applications.</p>
-      <dl>
-        <dt><a href="https://www.tenable.com/products/tenable-io" target="_blank" rel="noreferrer">Product Marketing Page</a></dt>
-        <dd>Tenable.io</dd>
-        <dd>
-          <ul>
-            <li>Worked with team members to develop first Tenable design system and component library</li>
-            <li>Created custom dashboard and reporting functionality including user-configured visualizations and drag and drop</li>
-            <li>Greenfield engineering for PCI ASV and MSSP features</li>
-          </ul>
-        </dd>
+        <Carousel>
+          <Slide>
+            <h4>@Tenable</h4>
+            <p>I was fortunate to have many opportunities at Tenable to bootstrap several new features and applications.</p>
+            <dl>
+              <dt><a href="https://www.tenable.com/products/tenable-io" target="_blank" rel="noreferrer">Product Marketing Page</a></dt>
+              <dd>Tenable.io</dd>
+              <dd>
+                <ul>
+                  <li>Worked with team members to develop first Tenable design system and component library</li>
+                  <li>Created custom dashboard and reporting functionality including user-configured visualizations and drag and drop</li>
+                  <li>Greenfield engineering for PCI ASV and MSSP features</li>
+                </ul>
+              </dd>
 
-        <dt><a href="https://www.tenable.com/products/tenable-sc" target="_blank" rel="noreferrer">Product Marketing Page</a></dt>
-        <dd>Tenable.sc</dd>
-        <dd>
-          <ul>
-            <li>Applied design system tokens to reskin existing UI components and deliver full application redesign for first major version release in eight years</li>
-            <li>Established standards for thorough code review and positive collaboration between disparate teams</li>
-          </ul>
-        </dd>
+              <dt><a href="https://www.tenable.com/products/tenable-sc" target="_blank" rel="noreferrer">Product Marketing Page</a></dt>
+              <dd>Tenable.sc</dd>
+              <dd>
+                <ul>
+                  <li>Applied design system tokens to reskin existing UI components and deliver full application redesign for first major version release in eight years</li>
+                  <li>Established standards for thorough code review and positive collaboration between disparate teams</li>
+                </ul>
+              </dd>
 
-        <dd>Tenable Cloud</dd>
-        <dd>
-          <ul>
-            <li>Worked to create API key generation and CAPTCHA features</li>
-            <li>Developed custom AsciiDoc to HTML conversion for online API documentation</li>
-            <li>Researched and prototyped native web components to vet the possibility of migrating</li>
-          </ul>
-        </dd>
-      </dl>
+              <dd>Tenable Cloud</dd>
+              <dd>
+                <ul>
+                  <li>Worked to create API key generation and CAPTCHA features</li>
+                  <li>Developed custom AsciiDoc to HTML conversion for online API documentation</li>
+                  <li>Researched and prototyped native web components to vet the possibility of migrating</li>
+                </ul>
+              </dd>
+            </dl>
+          </Slide>
 
-      <h4>@Apptio</h4>
-      <p>Most of my tenure was devoted to helping build Apptio's latest product.</p>
-      <dl>
-        <dt><a href="http://www.apptio.com/applications/it-planning" target="_blank" rel="noreferrer">Product Marketing Page</a></dt>
-        <dd>Apptio IT Planning Foundation</dd>
-        <dd>
-          <ul>
-            <li>Created prototype under direction of Director of Product Design and lead UX Designer, along with Visual Designer and another front end developer</li>
-            <li>Primary front end developer for tablet-up responsive web app</li>
-            <li>Filled the role of part engineer, part UX advocate/expert during development</li>
-            <li>Implemented Zurb's Foundation HTML/SCSS/CSS/JS library within Java backend</li>
-            <li>Learned basics of Knockout.js/Durandal.js data-bindings</li>
-            <li>Built various charts/diagrams using Highcharts and D3 SVG frameworks</li>
-          </ul>
-        </dd>
-      </dl>
+          <Slide>
+            <h4>@Apptio</h4>
+            <p>Most of my tenure was devoted to helping build Apptio's latest product.</p>
+            <dl>
+              <dt><a href="http://www.apptio.com/applications/it-planning" target="_blank" rel="noreferrer">Product Marketing Page</a></dt>
+              <dd>Apptio IT Planning Foundation</dd>
+              <dd>
+                <ul>
+                  <li>Created prototype under direction of Director of Product Design and lead UX Designer, along with Visual Designer and another front end developer</li>
+                  <li>Primary front end developer for tablet-up responsive web app</li>
+                  <li>Filled the role of part engineer, part UX advocate/expert during development</li>
+                  <li>Implemented Zurb's Foundation HTML/SCSS/CSS/JS library within Java backend</li>
+                  <li>Learned basics of Knockout.js/Durandal.js data-bindings</li>
+                  <li>Built various charts/diagrams using Highcharts and D3 SVG frameworks</li>
+                </ul>
+              </dd>
+            </dl>
+          </Slide>
 
-      <h4>@Sabre</h4>
-      <p>These are some projects I developed at Sabre Hospitality Solutions, though most have since been redeveloped.</p>
-      <dl>
-        <dt><a href="https://www.sandylane.com/barbados-golf/green-monkey-golf-course" target="_blank" rel="noreferrer">sandylane.com</a></dt>
-        <dd>Sandy Lane</dd>
-        <dd>St. James, Barbados, West Indies</dd>
-        <dd>
-          <ul>
-            <li>XML/XSLT website framework</li>
-            <li>jQuery scripting for scaling, automatic pagination and interactive golf course map</li>
-          </ul>
-        </dd>
+          <Slide>
+            <h4>@Sabre</h4>
+            <p>These are some projects I developed at Sabre Hospitality Solutions, though most have since been redeveloped.</p>
+            <dl>
+              <dt><a href="https://www.sandylane.com/barbados-golf/green-monkey-golf-course" target="_blank" rel="noreferrer">sandylane.com</a></dt>
+              <dd>Sandy Lane</dd>
+              <dd>St. James, Barbados, West Indies</dd>
+              <dd>
+                <ul>
+                  <li>XML/XSLT website framework</li>
+                  <li>jQuery scripting for scaling, automatic pagination and interactive golf course map</li>
+                </ul>
+              </dd>
 
-        <dd>Trump Hotel Collection</dd>
-        <dd>New York, New York</dd>
-        <dd>
-          <ul>
-            <li>Lead developer for project; served as company liaison between client and third party design firm to facilitate success</li>
-            <li>Integrated HTML/CSS with custom content management system</li>
-            <li>Developed PHP and XSLT template logic</li>
-          </ul>
-        </dd>
+              <dd>Trump Hotel Collection</dd>
+              <dd>New York, New York</dd>
+              <dd>
+                <ul>
+                  <li>Lead developer for project; served as company liaison between client and third party design firm to facilitate success</li>
+                  <li>Integrated HTML/CSS with custom content management system</li>
+                  <li>Developed PHP and XSLT template logic</li>
+                </ul>
+              </dd>
 
-        <dd>Destination Hotels & Resorts</dd>
-        <dd>Englewood, Colorado</dd>
-        <dd>
-          <ul>
-            <li>HTML/CSS front-end development</li>
-            <li>Primary developer for company's largest website project</li>
-          </ul>
-        </dd>
+              <dd>Destination Hotels &amp; Resorts</dd>
+              <dd>Englewood, Colorado</dd>
+              <dd>
+                <ul>
+                  <li>HTML/CSS front-end development</li>
+                  <li>Primary developer for company's largest website project</li>
+                </ul>
+              </dd>
 
-        <dd>Facebook Booking Widget</dd>
-        <dd>
-          <ul>
-            <li>Developed prototype version of Facebook App enabling users to book hotel reservations without leaving the Facebook website</li>
-            <li>Product had been replicated and sold nearly 200 times, generating $250K plus in revenue at the time of my departure</li>
-            <li>Client ROI was approximately 12:1 due to efficiency and speed of development allowing low price point</li>
-          </ul>
-        </dd>
-      </dl>
+              <dd>Facebook Booking Widget</dd>
+              <dd>
+                <ul>
+                  <li>Developed prototype version of Facebook App enabling users to book hotel reservations without leaving the Facebook website</li>
+                  <li>Product had been replicated and sold nearly 200 times, generating $250K plus in revenue at the time of my departure</li>
+                  <li>Client ROI was approximately 12:1 due to efficiency and speed of development allowing low price point</li>
+                </ul>
+              </dd>
+            </dl>
+          </Slide>
 
-      <h4>@Independent Consulting</h4>
+          <Slide>
+            <h4>@Independent Consulting</h4>
+            <p>Note: websites below have undergone redevelopment since my involvement, so I take no credit for them in their current state! Sample code can be provided if requested.</p>
+            <dl>
+              <dt><a href="http://www.advantageim.com/" target="_blank" rel="noreferrer">www.advantageim.com</a></dt>
+              <dd>Advantage Internet Marketing</dd>
+              <dd>Sykesville, MD</dd>
+              <dd>
+                <ul>
+                  <li>Promotional websites for local businesses with HTML, CSS and JS</li>
+                  <li>Simple guest book with ASP/SQL feature for the Girl Scouts of Central Maryland</li>
+                  <li>Programming using PHP/MySQL for form data processing</li>
+                </ul>
+              </dd>
 
-      <p>
-        Note: websites below have undergone redevelopment since my involvement, so I take no credit for them in their current state! Sample code can be provided if requested.
-      </p>
-      <dl>
-        <dt><a href="http://www.advantageim.com/" target="_blank" rel="noreferrer">www.advantageim.com</a></dt>
-        <dd>Advantage Internet Marketing</dd>
-        <dd>Sykesville, MD</dd>
-        <dd>
-          <ul>
-            <li>Promotional websites for local businesses with HTML, CSS and JS</li>
-            <li>Simple guest book with ASP/SQL feature for the Girl Scouts of Central Maryland</li>
-            <li>Programming using PHP/MySQL for form data processing</li>
-          </ul>
-        </dd>
+              <dt><a href="http://www.marylandlife.com" target="_blank" rel="noreferrer">www.marylandlife.com</a></dt>
+              <dd>Maryland Life Magazine</dd>
+              <dd>Frederick, Maryland</dd>
+              <dd>
+                <ul>
+                  <li>Worked directly with magazine publishing company President and Editor-in-Chief throughout project to ensure success</li>
+                  <li>Collaborated with freelance designer on website user interface redesign</li>
+                  <li>Developed front-end HTML and CSS templates</li>
+                </ul>
+              </dd>
 
-        <dt><a href="http://www.marylandlife.com" target="_blank" rel="noreferrer">www.marylandlife.com</a></dt>
-        <dd>Maryland Life Magazine</dd>
-        <dd>Frederick, Maryland</dd>
-        <dd>
-          <ul>
-            <li>Worked directly with magazine publishing company President and Editor-in-Chief throughout project to ensure success</li>
-            <li>Collaborated with freelance designer on website user interface redesign</li>
-            <li>Developed front-end HTML and CSS templates</li>
-          </ul>
-        </dd>
+              <dt><a href="http://www.thebaynet.com" target="_blank" rel="noreferrer">www.thebaynet.com</a></dt>
+              <dd>The Bay Net</dd>
+              <dd>Lexington Park, Maryland</dd>
+              <dd>
+                <ul>
+                  <li>Developed HTML, CSS and Javascript for local online news website</li>
+                  <li>Worked closely with proprietor, programmer and designer to redesign and redevelop website</li>
+                  <li>Coordinated CMS programming with back-end developers</li>
+                </ul>
+              </dd>
 
-        <dt><a href="http://www.thebaynet.com" target="_blank" rel="noreferrer">www.thebaynet.com</a></dt>
-        <dd>The Bay Net</dd>
-        <dd>Lexington Park, Maryland</dd>
-        <dd>
-          <ul>
-            <li>Developed HTML, CSS and Javascript for local online news website</li>
-            <li>Worked closely with proprietor, programmer and designer to redesign and redevelop website</li>
-            <li>Coordinated CMS programming with back-end developers</li>
-          </ul>
-        </dd>
+              <dt><a href="http://www.bangcartoon.com/" target="_blank" rel="noreferrer">www.bangcartoon.com</a></dt>
+              <dd>Bang! Cartoons</dd>
+              <dd>
+                <ul>
+                  <li>Designed and developed website for sports cartoonist and radio personality</li>
+                  <li>Gained design experience bringing artist's creative vision to life</li>
+                </ul>
+              </dd>
+            </dl>
+          </Slide>
+        </Carousel>
 
-        <dt><a href="http://www.bangcartoon.com/" target="_blank" rel="noreferrer">www.bangcartoon.com</a></dt>
-        <dd>Bang! Cartoons</dd>
-        <dd>
-          <ul>
-            <li>Designed and developed website for sports cartoonist and radio personality</li>
-            <li>Gained design experience bringing artist's creative vision to life</li>
-          </ul>
-        </dd>
-      </dl>
+        <p>
+          Note: websites below have undergone redevelopment since my involvement, so I take no credit for them in their current state! Sample code can be provided if requested.
+        </p>
       </ContentArticle>
     </main>
   </div>
@@ -152,4 +163,7 @@
 <script setup lang="ts">
 import ContentArticle from '../components/ContentArticle.vue'
 import HeroImage from '../components/HeroImage.vue'
+import Carousel from '../components/Carousel.vue'
+import Slide from '../components/Slide.vue'
+
 </script>
