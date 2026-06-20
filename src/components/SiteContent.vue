889fe74959d4920438dd118bf61d629adc4dcdde
Tavis Tucker <tavis.tucker@yahoo.com>
2026-06-19 23:51:51 -0400

    move the mixins to helpers file and migrate main styles to SiteContent

--- src/components/SiteContent.vue
+++ src/components/SiteContent.vue
@@ -3,3 +3,21 @@
     <slot />
   </main>
 </template>
+
+<style lang="scss">
+main {
+  position: relative;
+  width: 100%;
+}
+
+@include desktop-up {
+  main {
+    height: 100vh;
+    overflow-y: auto;
+    scrollbar-gutter: stable;
+  }
+  @include motion('forward', 105%, -115%, -220vh, -220vh);
+  @include motion('back', -105%, 115%, -220vh, 220vh);
+}
+
+</style>
